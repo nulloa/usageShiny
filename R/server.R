eec3c71a942a09e807244f0b2f04939ce193c0fd
Nehemias Ulloa <nehemiasulloa@gmail.com>
2024-08-24 07:53:12 -0700

    Switch app to tabs to add monthly data view

--- R/server.R
+++ R/server.R
@@ -3,18 +3,58 @@ library(tidyverse)
 
 function(input, output, session) {
   
+  ##### Setup for TAB 1 - Monthly Data #####
+  
   # Combine the selected variables into a new data frame
-  selectedData <- reactive({
-    electricityusagedata::hourly_usage %>%
-      filter(between(date, input$dateRange[1], input$dateRange[2])) %>%
+  selectedMonthlyData <- reactive({
+    electricityusagedata::monthly_usage %>%
+      filter(between(date, input$hourlyDateRange[1], input$hourlyDateRange[2])) %>%
       mutate(datetime = as.POSIXct(paste(date, start.time), format="%Y-%m-%d %H:%M:%S")) 
   })
   
-  output$plot1 <- renderPlot({
+  output$monthly_plot <- renderPlot({
     
-    timespan <- abs(input$dateRange[1] - input$dateRange[2])
+    timespan <- abs(input$monthlyDateRange[1] - input$monthlyDateRange[2])
     
-    mainplot <- ggplot(data=selectedData(), aes(x=datetime, y=consumption)) +
+    mainplot <- ggplot(data=selectedHourlyData(), aes(x=datetime, y=consumption)) +
+      geom_point() + 
+      geom_line() + 
+      theme_bw() +
+      labs(x="Date", y="Consumption (Kwh)")
+    
+    if(timespan < 2){
+      mainplot +
+        scale_x_datetime(date_breaks = "2 hour", date_labels = "%R")
+    }else{
+      if(timespan >= 2 & timespan < 7){
+        mainplot +
+          scale_x_datetime(date_breaks = "1 day", date_labels="%D\n%R")
+      }else{
+        mainplot +
+          scale_x_datetime(date_breaks = "1 week", date_labels="%D\n%R")
+      }
+    }
+    
+  })
+  
+  
+  
+  
+  
+  ##### Setup for TAB 2 - Hourly Data #####
+  
+  # Combine the selected variables into a new data frame
+  selectedHourlyData <- reactive({
+    electricityusagedata::hourly_usage %>%
+      filter(between(date, input$hourlyDateRange[1], input$hourlyDateRange[2])) %>%
+      mutate(datetime = as.POSIXct(paste(date, start.time), format="%Y-%m-%d %H:%M:%S")) 
+  })
+  
+  output$hourly_plot <- renderPlot({
+    
+    timespan <- abs(input$hourlyDateRange[1] - input$hourlyDateRange[2])
+    
+    mainplot <- ggplot(data=selectedHourlyData(), aes(x=datetime, y=consumption)) +
       geom_point() + 
       geom_line() + 
       theme_bw() +
--- R/ui.R
+++ R/ui.R
@@ -1,22 +1,47 @@
 
-mindate <- min(electricityusagedata::hourly_usage$date)
-maxdate <- max(electricityusagedata::hourly_usage$date)
+minHourDate <- min(electricityusagedata::hourly_usage$date)
+maxHourDate <- max(electricityusagedata::hourly_usage$date)
 
 fluidPage(
+  # Main Title
   titlePanel("Hourly Electricity Usage Data."),
   
-  fluidRow(
-    column(4,
-      dateRangeInput('dateRange',
-                     label = 'Choose a date. (yyyy-mm-dd)',
-                     start = as.Date("2024-08-01") - 1, 
-                     end = as.Date("2024-08-01") + 1,
-                     min = mindate,
-                     max = maxdate
-                     )
-      )),
+  # Set Color/Font Scheme/Theme
+  # theme = shinythemes::shinytheme("lumen"),
   
-  plotOutput('plot1')
+  # Setup TABs
+  tabsetPanel(
+    # TAB 1
+    tabPanel("Monthly Usage",
+             fluidRow(
+               column(4,
+                      dateRangeInput('dateRange',
+                                     label = 'Choose a date. (yyyy-mm-dd)',
+                                     start = as.Date("2024-08-01") - 1, 
+                                     end = as.Date("2024-08-01") + 1,
+                                     min = mindate,
+                                     max = maxdate
+                      )
+               )),
+             
+             plotOutput('plot1')
+    ),
+    # TAB 2
+    tabPanel("Monthly Usage",
+             fluidRow(
+               column(4,
+                      dateRangeInput('hourlyDateRange',
+                                     label = 'Choose a date. (yyyy-mm-dd)',
+                                     start = as.Date("2024-08-01") - 1, 
+                                     end = as.Date("2024-08-01") + 1,
+                                     min = minHourDate,
+                                     max = maxHourDate
+                      )
+               )),
+             
+             plotOutput('hourly_plot')
+    )
+  )
 )
 
 
